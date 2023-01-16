e9655689d7efe22a19cc6051a809206f9a2b4572
stoshich <stoshich.stefan@yandex.ru>
2023-01-16 15:44:40 +0300

    created project vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+  @import './styles/global.scss';
+  #app {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Добро пожаловать в UI-Kit
+    </h1>
+</template>
