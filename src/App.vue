ea1c1083cbb984d49564532f75dc5498976aa8f7
stoshich <stoshich.stefan@yandex.ru>
2023-01-16 22:06:35 +0300

    added layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
+import LayoutHeader from '@/components/layout/Header.vue'
+import LayoutSidebar from '@/components/layout/Sidebar.vue'
+import { ref } from '@vue/reactivity'
+
+const isOpenMenu = ref(false)
+const toggleMenu = () => {
+  isOpenMenu.value = !isOpenMenu.value
+}
 </script>
 
 <template>
-  <router-view/>
+  <div class="container">
+    <div class="sidebar-toggle" @click="toggleMenu">&#5125;</div>
+    <layout-header/>
+    <layout-sidebar :openSidebar="isOpenMenu"/>
+    <div :class="['content', {content_full: !isOpenMenu}]">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -12,4 +27,32 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+  .content {
+    max-width: 1400px;
+    margin-left: 250px;
+    padding: 30px;
+    transition: .2s;
+    &_full {
+      margin-left: 0;
+    }
+  }
+  .sidebar-toggle {
+  position: fixed;
+  left: 0;
+  width: 15px;
+  background: var(--primary-color);
+  height: 100%;
+  top: 62px;
+  z-index: 1;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+@media screen and (max-width: 1023px) {
+  .content {
+    margin-left: 0;
+  }
+}
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>
+    <h1 class="heading-1">
         Добро пожаловать в UI-Kit
     </h1>
 </template>
