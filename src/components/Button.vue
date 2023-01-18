7ab93bf99987a3beb892ecd15703d11cb1342165
stoshich <stoshich.stefan@yandex.ru>
2023-01-18 15:43:20 +0300

    added more buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -7,12 +7,46 @@
         color: {
             type: String,
             default: 'primary'
+        },
+        disabled: {
+            type: Boolean,
+            required: false
+        },
+        rounded: {
+            type: Boolean,
+            required: false
+        },
+        outlined: {
+            type: Boolean,
+            required: false
+        },
+        icon: {
+            type: String,
+            required: false
+        },
+        size: {
+            type: String,
+            default: 'normal'
         }
     })
+
+    const emit = defineEmits(['click'])
+    const clickOnButton = () => {
+        emit('click')
+    }
 </script>
 
 <template>
-    <button :class="['btn', `btn_${color}`]">{{ label }}</button>
+    <button 
+    :class="['btn', `btn_${color}`, {'btn_rounded' : rounded}, {'btn_outlined' : outlined}, {'btn_icon' : icon}, {'btn_large' : size === 'large'}]"
+    :disabled="disabled"
+    @click="clickOnButton"
+    >
+        <span v-if="icon">
+            <font-awesome-icon :icon="`fa-regular fa-${icon}`" />
+        </span>
+        <span v-else>{{ label }}</span>
+    </button>
 </template>
 
 <style lang="scss" scoped>
@@ -68,6 +102,30 @@
     &:enabled:hover {
         background: var(--danger-hover);
     }
+    }
+    &:disabled {
+    opacity: .6;
+    cursor: default;
+    }
+    &_rounded {
+    border-radius: 15px;
+    }
+    &_outlined {
+    background: transparent;
+    color: #000;
+    &:hover {
+        color: #fff;
+    }
+    }
+    &_icon {
+    padding: 0;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    }
+    &_large {
+    height: 48px;
+    padding: 0 30px;
     }
 }
 
--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,9 +1,18 @@
 <script setup>
     import Button from '@/components/Button.vue'
+
+    const eventForButton = () => {
+        alert('You clicked on button')
+    }
 </script>
 
 <template>
     <h1 class="heading-1">Button</h1>
+    <h2 class="heading-2">Disabled</h2>
+    <div class="line">
+        <Button label="Primary" color="primary" :disabled="true"/>
+        <Button label="Secondary" color="second" :disabled="true"/>
+    </div>
     <h2 class="heading-2">Colors</h2>
     <div class="line">
         <Button label="Primary" color="primary"/>
@@ -12,6 +21,42 @@
         <Button label="Info" color="info"/>
         <Button label="Warning" color="warning"/>
         <Button label="Danger" color="danger"/>
+    </div>
+    <h2 class="heading-2">Rounded</h2>
+    <div class="line">
+        <Button label="Primary" color="primary" :rounded="true"/>
+        <Button label="Secondary" color="second" :rounded="true"/>
+        <Button label="Success" color="success" :rounded="true"/>
+        <Button label="Info" color="info" :rounded="true"/>
+        <Button label="Warning" color="warning" :rounded="true"/>
+        <Button label="Danger" color="danger" :rounded="true"/>
+    </div>
+    <h2 class="heading-2">Outlined</h2>
+    <div class="line">
+        <Button label="Primary" color="primary" :outlined="true"/>
+        <Button label="Secondary" color="second" :outlined="true"/>
+        <Button label="Success" color="success" :outlined="true"/>
+        <Button label="Info" color="info" :outlined="true"/>
+        <Button label="Warning" color="warning" :outlined="true"/>
+        <Button label="Danger" color="danger" :outlined="true"/>
+    </div>
+    <h2 class="heading-2">Icons</h2>
+    <div class="line">
+        <Button color="primary" icon="heart" :outlined="true"/>
+        <Button color="second" icon="hand"/>
+        <Button color="success" icon="address-book" :outlined="true"/>
+        <Button color="info" icon="credit-card"/>
+        <Button color="warning" icon="lemon" :outlined="true"/>
+        <Button color="danger" icon="hourglass"/>
+    </div>
+    <h2 class="heading-2">Size</h2>
+    <div class="line">
+        <Button label="Normal" color="primary"/>
+        <Button label="Large" color="primary" size="large"/>
+    </div>
+    <h2 class="heading-2">Event</h2>
+    <div class="line">
+        <Button label="@click" color="danger" @click="eventForButton"/>
     </div>
 </template>
 
