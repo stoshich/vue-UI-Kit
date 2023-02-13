9cf8ab2aa31f24ce3a8d23c9860182254fe165bd
stoshich <stoshich.stefan@yandex.ru>
2023-02-13 21:11:20 +0300

    tabs

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -13,7 +13,8 @@ const links = ref([
     {name: 'Checkbox', href: '/checkbox'},
     {name: 'Radiobutton', href: '/radiobutton'},
     {name: 'Progress', href: '/progress'},
-    {name: 'Input', href: '/input'}
+    {name: 'Input', href: '/input'},
+    {name: 'Tabs', href: '/tabs'},
 ])
 
 </script>
---
+++ src/views/Tabs.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import Tabs from '@/components/Tabs.vue'
+import { ref } from 'vue';
+
+  const tabs = [
+    {name: 'About', label: 'О проекте'},
+    {name: 'Vue', label: 'О Vue'},
+    {name: 'React', label: 'О React'},
+  ]
+  const selectedTab = ref('Vue')
+  const changeTab = (tabName) => {
+    selectedTab.value = tabName
+  }
+</script>
+
+<template>
+  <h1 class="heading-1">Tabs</h1>
+  <Tabs
+    :names="tabs"
+    :selectedTab="selectedTab"
+    @changeTab="changeTab"
+  >
+  <div v-if="selectedTab === 'About'">
+      Lorem ipsum dolor sit amet.
+    </div>
+    <div v-if="selectedTab === 'Vue'">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, dolor?
+    </div>
+    <div v-if="selectedTab === 'React'">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias modi in placeat. Illo, deserunt tenetur!
+    </div>
+  </Tabs>
+</template>
